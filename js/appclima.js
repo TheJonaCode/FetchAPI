429681afbbb47ba1f6faa493fb3369eeb164f1a6
Jonathan <mcjhona069@gmail.com>
2020-09-15 17:55:43 -0500

    Validando el formulario

--- js/appclima.js
+++ js/appclima.js
@@ -10,6 +10,41 @@ function buscarClima(e){
     e.preventDefault();
 
     // VALIDAR
+    const ciudad = document.querySelector('#ciudad').value;
+    const pais = document.querySelector('#pais').value;
+    if(ciudad === '' || pais == ''){
+        //Hubo un error
+        mostrarError('Ambos campos son obligatorios');
+
+        return;
+    }
 
     // Consultar la API
+}
+
+function mostrarError(mensaje){
+    const alerta = document.querySelector('.bg-red-100');
+
+    if(!alerta){
+
+       //CREAR UNA ALERTA
+      const alerta = document.createElement('div');
+
+      alerta.classList.add('bg-red-100', 'border-red-400', 'text-red-700', 'px-4', 'py-3', 'rounded',
+      'max-w-md', 'mx-auto', 'mt-6', 'text-center');
+
+      alerta.innerHTML = `
+             <strong class ="font-bold">Error!</strong>
+             <span class ="block">${mensaje}</span>
+            `;
+
+        container.appendChild(alerta);
+
+        // HACER QUE SE ELIMINE LA ALERTA
+        setTimeout(() =>{
+            alerta.remove();
+        }, 5000);
+    }
+
+    
 }
