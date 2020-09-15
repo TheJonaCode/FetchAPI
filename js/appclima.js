5c9f24b9234357324e1c16e40b1bd063574fca2e
Jonathan <mcjhona069@gmail.com>
2020-09-15 18:17:56 -0500

    Obtener una API Key

--- js/appclima.js
+++ js/appclima.js
@@ -20,6 +20,8 @@ function buscarClima(e){
     }
 
     // Consultar la API
+    consultarAPI(ciudad,pais);
+
 }
 
 function mostrarError(mensaje){
@@ -46,5 +48,21 @@ function mostrarError(mensaje){
         }, 5000);
     }
 
-    
+
+}
+
+function consultarAPI(ciudad,pais) {
+
+    const  appId = '4ada61b568d9c41d0562e9a78159362f';
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
+
+    fetch(url)
+        .then(respuesta => respuesta.json())
+        .then( datos => {
+            console.log(datos);
+            if(datos.cod === "404"){
+                mostrarError('Ciudad no encontrada')
+            }
+        })
 }
