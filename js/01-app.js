78d6265e6d701312edb19fe8d8255be88eddbc39
Jonathan <mcjhona069@gmail.com>
2020-09-15 14:26:30 -0500

    Como utilizar Fetch API

---
+++ js/01-app.js
@@ -0,0 +1,25 @@
+const cargarTextBtn = document.querySelector('#cargarTxt');
+cargarTextBtn.addEventListener('click', obtenerDatos);
+
+function obtenerDatos() {
+    /* DECLARAR FECTH FORMA 1
+    fetch('data/datos.txt' */
+    //FORMA 2
+    const url = 'data/datos.txt';
+    fetch(url)
+        .then( respuesta => {
+            console.log(respuesta);
+            console.log(respuesta.status);
+            console.log(respuesta.statusText);
+            console.log(respuesta.url);
+            console.log(respuesta.type);
+
+            return respuesta.text()
+        })
+        .then(datos =>{
+            console.log(datos)
+        })
+        .catch(error =>{
+            console.log(error);
+        })
+}
